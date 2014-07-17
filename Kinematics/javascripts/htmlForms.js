13ab955f10d3f9ee7e0e1398e615cb52e2025b58
Steve <steven.p.gantz@gmail.com>
2014-07-17 12:15:02 -0400

    Create htmlForms.js
    created javascript form manipulation template

---
+++ Kinematics/javascripts/htmlForms.js
@@ -0,0 +1,31 @@
+function userChoseInitialPosition() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedA.";
+}
+
+function userChoseFinalPosition() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedB.";
+}
+
+function userChoseVelocity() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedC.";
+}
+
+function userChoseTime() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedD.";
+}
+
+function userChoseAcceleration() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedE.";
+}
+
+function userChoseInitialVelocity() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedF.";
+}
+
+function userChoseFinalVelocity() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedG";
+}
+
+function userChoseDistance() {
+    document.getElementById("mainForm").innerHTML = "Paragraph changedH";
+}
